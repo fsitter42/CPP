91b6536c46bff377ad098fbf33d059235661e392
forkroyal <gfabits12@gmail.com>
2026-04-25 14:47:53 +0200

    suwo

--- 00md/ex01/phonebook.cpp
+++ 00md/ex01/phonebook.cpp
@@ -6,13 +6,48 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 13:39:11 by fsitter           #+#    #+#             */
-/*   Updated: 2026/04/25 13:41:34 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/04/25 14:25:37 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-int	main(int argc, char *argv[])
+class Contact
 {
-	
+  private:
+	int index;
+	std::string first_name;
+	std::string last_name;
+	std::string nick_name;
+	std::string phonenumber;
+	std::string darkest_secrect;
+
+  public:
+	std::string getFname(int index)
+	{
+		
+		return first_name;
+	}
+};
+
+class PhoneBook
+{
+  private:
+	Contact contacts[8];
+	int _count;
+
+  public:
+	void printContact(int index)
+	{
+		std::cout << contacts->first_name[index];
+	};
+};
+
+int	main(void)
+{
+	std::string greeting;
+	std::string intro;
+	greeting = "Hello and welcome to your new Phonebook.\n";
+	intro = "Enter one of three commands. The program only accepts ADD, SEARCH and EXIT.";
+	std::cout << greeting << intro << std::endl;
 }
