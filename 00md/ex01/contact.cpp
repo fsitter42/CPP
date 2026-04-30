0e3ce8f966e656697500a0cf339280febf603695
fsitter42 <fsitter@student.42vienna.com>
2026-05-01 00:26:21 +0200

    contact.hpp

--- 00md/ex01/contact.cpp
+++ 00md/ex01/contact.cpp
@@ -6,23 +6,56 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 13:39:11 by fsitter           #+#    #+#             */
-/*   Updated: 2026/04/27 16:29:27 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/05/01 00:23:58 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "contact.hpp"
 
-Contact::Contact(int i, std::string fn, std::string ln, std::string nn, std::string pn, std::string ds) : index(i), first_name(fn), last_name(ln), nick_name(nn), phonenumber(pn), darkest_secrect(ds) 
-{
-	
-}
+Contact::Contact() {}
 
-std::string Contact::getFirstName() const
+const std::string& Contact::getFirstName() const
 {
 	return first_name;
-};
+}
 
-std::string Contact::getLastName() const
+const std::string& Contact::getLastName() const
 {
 	return last_name;
-};
+}
+
+const std::string& Contact::getNickName() const
+{
+	return nick_name;
+}
+
+const std::string& Contact::getPhoneNum() const
+{
+	return phonenumber;
+}
+
+const std::string& Contact::getSecrect() const
+{
+	return darkest_secrect;
+}
+
+void Contact::setFirstName(const std::string& fn)
+{
+	first_name = fn;
+}
+void Contact::setLastName(const std::string& ln)
+{
+	last_name = ln;
+}
+void Contact::setNickName(const std::string& nn)
+{
+	nick_name = nn;
+}
+void Contact::setPhoneNum(const std::string& pn)
+{
+	phonenumber = pn;
+}
+void Contact::setSecrect(const std::string& ds)
+{
+	darkest_secrect = ds;
+}
--- 00md/ex01/contact.hpp
+++ 00md/ex01/contact.hpp
@@ -6,20 +6,18 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 13:38:31 by fsitter           #+#    #+#             */
-/*   Updated: 2026/04/27 16:29:41 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/05/01 00:25:13 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
-#include <cctype>
-#include <iostream>
+#include <string>
 
 class Contact
 {
   private:
-	int index;
 	std::string first_name;
 	std::string last_name;
 	std::string nick_name;
@@ -27,9 +25,17 @@ class Contact
 	std::string darkest_secrect;
 
   public:
-  	Contact (int i, std::string fn, std::string ln, std::string nn, std::string pn, std::string ds);
-	std::string getFirstName() const;
-	std::string getLastName() const;
+  	Contact ();
+	const std::string& getFirstName() const;
+	const std::string& getLastName() const;
+	const std::string& getNickName() const;
+	const std::string& getPhoneNum() const;
+	const std::string& getSecrect() const;
+	void setFirstName(const std::string& fn);
+	void setLastName(const std::string& ln);
+	void setNickName(const std::string& nn);
+	void setPhoneNum(const std::string& pn);
+	void setSecrect(const std::string& ds);
 };
 
 #endif
