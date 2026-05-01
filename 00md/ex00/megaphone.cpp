1ef15cd7be3eff85a594c34e994e54b38b0f3362
fs42 <fsitter42@fsitter.com>
2026-05-02 01:13:27 +0200

    suwi

--- 00md/ex00/megaphone.cpp
+++ 00md/ex00/megaphone.cpp
@@ -5,22 +5,50 @@ int	main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
-	    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	    return (0);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
 	}
-	size_t i = 1;
-	size_t ac = static_cast<size_t>(argc);
+	size_t	i = 1;
+	size_t	ac = static_cast<size_t>(argc);
 	while (i < ac)
 	{
-	    size_t j = 0;
-	    while (argv[i][j])
+		size_t	j = 0;
+		while (argv[i][j])
 		{
-	        std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
-	        j++;
-	    }
-	    if (i < ac - 1)
-	        std::cout << " ";
-	    i++;
+			std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
+			j++;
+		}
+		if (i < ac - 1)
+			std::cout << " ";
+		i++;
 	}
 	std::cout << '\n';
+	return (0);
 }
+
+/*
+#include <cctype>
+#include <iostream>
+#include <span> // c++20
+#include <string_view> // c++17
+
+int main (int argc, char *argv[])
+{
+	if (argc == 1) 
+	{
+    	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    	return (0);
+	}
+	auto args = std::span(argv + 1, argc - 1);
+	for (std::string_view arg : args.first(args.size() - 1))
+	{
+    	for (char c : arg)
+        	std::cout << (char)std::toupper((unsigned char)c);
+    	std::cout << ' ';
+	}
+	for (char c : args.back())
+   		std::cout << (char)std::toupper((unsigned char)c);
+	std::cout << '\n';
+	return (0);
+}
+*/
