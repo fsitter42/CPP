510f9b472d266f6f53738336ed7ef8c809f6f1a8
fsitter42 <fsitter@student.42vienna.com>
2026-05-01 13:26:47 +0200

    addContactmain

--- 00md/ex01/main.cpp
+++ 00md/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 13:39:11 by fsitter           #+#    #+#             */
-/*   Updated: 2026/05/01 01:45:33 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/05/01 13:26:24 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,63 @@
 #include <iostream>
 #include <iomanip>
 
+void greeting(void);
+static std::string getInput(const std::string& prompt);
+static void addContact(PhoneBook& pb);
 
 int	main(void)
+{
+	PhoneBook pb;
+	std::string input;
+	
+	greeting();
+	while (true)
+	{
+		
+		std::cin >> input;
+		std::cin.ignore();
+
+		if (input == "ADD")
+			addContact(pb);
+		else if (input == "SEARCH")
+		{
+			
+		}
+		else if (input == "EXIT")
+			break;
+	}
+	return (0);	
+}
+
+void greeting(void)
 {
 	std::string greeting;
 	std::string intro;
 	greeting = "Hello and welcome to your new Phonebook.\n";
-	intro = "Enter one of three commands. The program only accepts ADD, SEARCH and EXIT.";
-	std::cout << greeting << intro << std::endl;
+	intro = "Enter one of three commands. The program only accepts ADD, SEARCH and EXIT.\n";
+	std::cout << greeting << intro;
+}
 
-
-	while (true)
+static std::string getInput(const std::string& prompt)
+{
+	std::string input;
+	do
 	{
-		std::string input;
-		std::cin >> input;
-		break;
-	}
-	
-	
+		std::cout << prompt;
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "Field cannot be empty.\n";
+	} while (input.empty());
+	return (input);	
+}
+
+static void addContact(PhoneBook& pb)
+{
+	std::string fn, ln, nn, pn, ds;
+	fn = getInput("First Name: ");
+	ln = getInput("Last Name: ");
+	nn = getInput("Nick Name: ");
+	pn = getInput("Phone Number: ");
+	ds = getInput("Darkest Secret: ");
+	pb.addContact(fn, ln, nn, pn, ds);
 }
