0c80c717abda27730a4da93584f4e7f329bfc2dd
fs42 <fsitter42@fsitter.com>
2026-05-05 23:38:13 +0200

    suwi

--- 00md/ex02/Account.cpp
+++ 00md/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: a600 <a600@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 12:36:54 by fsitter           #+#    #+#             */
-/*   Updated: 2026/05/05 23:08:56 by a600             ###   ########.fr       */
+/*   Updated: 2026/05/05 23:30:22 by a600             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,26 @@
 #include <sys/time.h>
 #include <iostream>
 
+void Account::_displayTimestamp(void)
+{
+	struct timeval  tv;
+    struct tm       *tm;
+    char            buf[20];
+
+    gettimeofday(&tv, NULL);
+    tm = localtime(&tv.tv_sec);
+    strftime(buf, sizeof(buf), "%Y%m%d_%H%M%S", tm);
+    std::cout << "[" << buf << "] ";
+}
+
 Account::Account( int initial_deposit )
 {}
 
 Account::~Account( void )
 {}
 
-/*
- * private:
- * */
-
-static void _displayTimestamp(void)
+int main()
 {
-	std::cout << gettimeofday();
+	Account aa(1);
+	Account::_displayTimestamp();
 }
