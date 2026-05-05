eba2c947f4b39da17ac03480086c554ac33adf3b
fs42 <fsitter42@fsitter.com>
2026-05-05 23:45:58 +0200

    suwi

--- 00md/ex02/Account.cpp
+++ 00md/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: a600 <a600@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 12:36:54 by fsitter           #+#    #+#             */
-/*   Updated: 2026/05/05 23:42:07 by a600             ###   ########.fr       */
+/*   Updated: 2026/05/05 23:45:45 by a600             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ void Account::_displayTimestamp(void)
 
 Account::Account( int initial_deposit )
 {
-	_accountIndex = _nbAccounts;
-	_amount = initial_deposit;
-	_nbDeposits = 0;
-	_nbWithdrawals = 0;
-	_nbAccounts++;
-	_totalAmount += initial_deposit;
+	Account::_accountIndex = _nbAccounts;
+	Account::_amount = initial_deposit;
+	Account::_nbDeposits = 0;
+	Account::_nbWithdrawals = 0;
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
 	_displayTimestamp();
-	std::cout << "\n";
+	std::cout << "index:" << Account::_accountIndex << "\n";
 	//[19920104_091532] index:0;amount:42;created
 }
 
