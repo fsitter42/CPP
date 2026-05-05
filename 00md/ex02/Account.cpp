f90d2047fd3f6575f79dd603c377d9ad713bde91
fs42 <fsitter42@fsitter.com>
2026-05-05 23:47:36 +0200

    suwi

--- 00md/ex02/Account.cpp
+++ 00md/ex02/Account.cpp
@@ -6,13 +6,18 @@
 /*   By: a600 <a600@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 12:36:54 by fsitter           #+#    #+#             */
-/*   Updated: 2026/05/05 23:45:45 by a600             ###   ########.fr       */
+/*   Updated: 2026/05/05 23:47:23 by a600             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <sys/time.h>
 #include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 void Account::_displayTimestamp(void)
 {
@@ -28,14 +33,14 @@ void Account::_displayTimestamp(void)
 
 Account::Account( int initial_deposit )
 {
-	Account::_accountIndex = _nbAccounts;
-	Account::_amount = initial_deposit;
-	Account::_nbDeposits = 0;
-	Account::_nbWithdrawals = 0;
-	Account::_nbAccounts++;
-	Account::_totalAmount += initial_deposit;
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
 	_displayTimestamp();
-	std::cout << "index:" << Account::_accountIndex << "\n";
+	std::cout << "index:" << _accountIndex << "\n";
 	//[19920104_091532] index:0;amount:42;created
 }
 
