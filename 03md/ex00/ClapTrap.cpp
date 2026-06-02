6a3a576aa1ab3930495ac7ed8a5074fad6b890c4
fsitter42 <fsitter@student.42vienna.com>
2026-06-02 15:56:17 +0200

    suwi

--- 03md/ex00/ClapTrap.cpp
+++ 03md/ex00/ClapTrap.cpp
@@ -1,6 +1,39 @@
 #include "ClapTrap.hpp"
+#include <iostream>
 
-ClapTrap::ClapTrap() : _name("")
+ClapTrap::ClapTrap() : _name(" "), _hit(10), _energy(10), _dmg(0)
+{
+    std::cout << "Default constructor called\n";
+}
+
+ClapTrap::ClapTrap(std::string name) : _hit(10), _energy(10), _dmg(0)
+{
+    _name = name;
+    std::cout << "Name constructor called\n";
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other) : _name(other._name), _hit(other._hit), _energy(other._energy), _dmg(other._dmg)
+{
+    std::cout << "Copy constructor called\n";
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+    std::cout << "Copy assignment operator called\n";
+    if (this != &other)
+    {
+        _name = other._name;
+        _hit = other._hit;
+        _energy = other._energy;
+        _dmg = other._dmg;
+    }
+    return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "Destructor called\n";
+}
 
 void ClapTrap::attack(const std::string& target)
 {
