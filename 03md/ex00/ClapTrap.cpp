e2e204a043ad99091ceddb2e86ca2a4aa03518dd
fsitter42 <fsitter@student.42vienna.com>
2026-06-02 15:47:46 +0200

    suwi

--- 03md/ex00/ClapTrap.cpp
+++ 03md/ex00/ClapTrap.cpp
@@ -1,9 +1,13 @@
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap() : _name("")
+
 void ClapTrap::attack(const std::string& target)
 {
+    // if _hit && _energy > 0
     // does this->_dmg to target (how to i reduce _hit in target?)
     // cost 1 _energy
+    // std::cout ClapTrap <name> attacks <target>, causing <damage> points of damage!
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
@@ -13,5 +17,7 @@ void ClapTrap::takeDamage(unsigned int amount)
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-    // 
+    // if _hit && _energy > 0
+    // this->_hit += amount
+    // costs 1 energy
 }
--- 03md/ex00/ClapTrap.hpp
+++ 03md/ex00/ClapTrap.hpp
@@ -13,6 +13,10 @@ class ClapTrap
 
   public:
       ClapTrap();
+      ClapTrap(std::string name);
+      ClapTrap(const ClapTrap& other);
+      ClapTrap& operator=(const ClapTrap& other);
+      ~ClapTrap();
       void attack(const std::string& target);
       void takeDamage(unsigned int amount);
       void beRepaired(unsigned int amount);
