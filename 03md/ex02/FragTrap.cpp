141b5531c05ca172dd713d977154e866f4466ef5
fsitter42 <fsitter@student.42vienna.com>
2026-06-08 10:56:12 +0200

    declared FragTrap.cpp

---
+++ 03md/ex02/FragTrap.cpp
@@ -0,0 +1,38 @@
+#include "FragTrap.hpp"
+#include "ClapTrap.hpp"
+#include "iostream"
+
+FragTrap::FragTrap() : ClapTrap()
+{
+    std::cout << "FT Default constructor called\n";
+}
+
+FragTrap::FragTrap(const std::string& name) : ClapTrap(name)
+{
+    std::cout << "FT Name constructor called\n";
+    _hit = 100;
+    _energy = 100;
+    _dmg = 30;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
+{
+    std::cout << "FT Copy constructor called\n";
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other)
+{
+    if (this != &other)
+        ClapTrap::operator=(other);
+    return (*this);
+}
+
+FragTrap::~FragTrap()
+{
+    std::cout << "FT Destructor called\n";
+}
+
+void FragTrap::highFiveGuys(void)
+{
+    std::cout << "FT positive high-five message.\n ";
+}
--- 03md/ex02/FragTrap.hpp
+++ 03md/ex02/FragTrap.hpp
@@ -10,7 +10,7 @@ class FragTrap : public ClapTrap
         FragTrap();
         FragTrap(const std::string& name);
         FragTrap(const FragTrap& other);
-        FragTrap& operator= (const FragTrap& other);
+        FragTrap& operator=(const FragTrap& other);
         ~FragTrap();
         void highFiveGuys(void);        
 };
