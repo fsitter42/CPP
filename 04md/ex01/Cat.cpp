db6388466b330ddfaae660dd0f7a89280cb82df8
fsitter42 <fsitter@student.42vienna.com>
2026-06-15 15:28:03 +0200

    updated cat.hpp.cpp for brain

--- 04md/ex01/Cat.cpp
+++ 04md/ex01/Cat.cpp
@@ -5,24 +5,31 @@ Cat::Cat() : Animal()
 {
     std::cout << "Cat Default Constructor called.\n";
     _type = "Cat";
+    _brain = new Brain();
 }
 
 Cat::Cat(const Cat& other) : Animal(other)
 {
     std::cout << "Cat Copy Constructor called.\n";
+    _brain = new Brain(*other._brain);
 }
 
 Cat& Cat::operator=(const Cat& other)
 {
     std::cout << "Cat Copy Assignment Operator called.\n";
     if (this != &other)
+    {
         Animal::operator=(other);
+        delete _brain;
+        _brain = new Brain(*other._brain);
+    }
     return (*this);
 }
 
 Cat::~Cat()
 {
     std::cout << "Cat Destructor called.\n";
+    delete _brain;
 }
 
 void Cat::makeSound() const
