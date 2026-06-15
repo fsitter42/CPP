2413e83525ee6ea27a601e77eb9ac2eaa6a2492a
fsitter42 <fsitter@student.42vienna.com>
2026-06-15 15:26:05 +0200

    updated dog.hpp/cpp for brain

--- 04md/ex01/Dog.cpp
+++ 04md/ex01/Dog.cpp
@@ -5,24 +5,31 @@ Dog::Dog() : Animal()
 {
     std::cout << "Dog Default Constructor called.\n";
     _type = "Dog";
+    _brain = new Brain();
 }
 
 Dog::Dog(const Dog& other) : Animal(other)
 {
     std::cout << "Dog Copy Constructor called.\n";
+    _brain = new Brain(*other._brain);
 }
 
 Dog& Dog::operator=(const Dog& other)
 {
     std::cout << "Dog Copy Assignment Operator called.\n";
     if (this != &other)
+    {
         Animal::operator=(other);
+        delete _brain;
+        _brain = new Brain(*other._brain);
+    }
     return (*this);
 }
 
 Dog::~Dog()
 {
     std::cout << "Dog Destructor called.\n";
+    delete _brain;
 }
 
 void Dog::makeSound() const
