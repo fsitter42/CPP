800d410386ffa4480d00c7fc9eadd923e14cd74c
fsitter42 <fsitter@student.42vienna.com>
2026-06-23 15:22:32 +0200

    Cure.hpp Cure.cpp + Makefile added

---
+++ 04md/ex03/Cure.cpp
@@ -0,0 +1,38 @@
+#include "Cure.hpp"
+#include "ICharacter.hpp"
+#include <iostream>
+
+Cure::Cure() : AMateria("cure")
+{
+    std::cout << "Cure Default Constructor called\n";
+}
+
+Cure::Cure(const Cure& other) : AMateria(other)
+{
+    std::cout << "Cure Copy Constructor called\n";
+}
+
+Cure& Cure::operator=(const Cure& other)
+{
+    if (this != &other)
+    {
+        this->_type = other._type;
+    }
+    return (*this);
+}
+
+Cure::~Cure()
+{
+    std::cout << "Cure Destructor called\n";
+}
+
+ AMateria* Cure::clone() const
+ {
+     AMateria *a = new Cure();
+     return (a);
+ }
+
+ void Cure::use(ICharacter& target)
+ {
+     std::cout << "* heals " << target.getName() << "'s wounds *\n";
+ }
