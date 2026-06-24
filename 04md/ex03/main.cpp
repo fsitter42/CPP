102e9c835ddfb88ee5c6b048b2be4c3437cbbc8b
fsitter42 <fsitter@student.42vienna.com>
2026-06-24 07:23:16 +0200

    added my tests for module 4 ex3

--- 04md/ex03/main.cpp
+++ 04md/ex03/main.cpp
@@ -5,22 +5,46 @@
 #include "Character.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
+#include <iostream>
 
 void md04test();
 void mytest();
 
 int main()
 {
-    //md04test();
+    md04test();
+    std::cout << "\n\n\n\n";
     mytest();
     
 }
 
 void mytest()
 {
-    Character b("hundi");
-    //MateriaSource a;
+    Character a("hundi");
+    std::cout << "my name is " << a.getName() << "\n";
+    MateriaSource b;
+    AMateria* ice = new Ice;
+    a.equip(ice);
+    a.use(0, a);
+    Character c("hansi");
+    c = a;
+    std::cout << "my name is " << c.getName() << "\n";
+    a.unequip(0);
+    c.use(0, c);
+    Character d(c);
+    c.unequip(0);
+    Character f("bello");
+    d.use(0, f);
+
+
+    AMateria* ice2 = new Ice;
+    b.learnMateria(ice2);
+
+    AMateria* ice3 = b.createMateria("ice");
+
+    d.equip(ice3);
     
+    delete ice;
 }
 
 void md04test()
