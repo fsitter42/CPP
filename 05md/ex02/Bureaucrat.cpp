eba0e83187c4c6ea785e68396c39f70de8bdd882
fsitter42 <fsitter@student.42vienna.com>
2026-09-12 20:27:37 +0200

    Notes

--- 05md/ex02/Bureaucrat.cpp
+++ 05md/ex02/Bureaucrat.cpp
@@ -13,17 +13,17 @@ int Bureaucrat::isValidGrade_(int grade) const
 
 Bureaucrat::Bureaucrat() : name_("Boai"), grade_(_worstGrade)
 {
-	std::cout << "Default Contructor called\n";
+	std::cout << "Bureaucrat Default Contructor called\n";
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : name_(other.name_), grade_(other.grade_)
 {
-	std::cout << "Copy Constructor called\n";
+	std::cout << "Bureaucrat Copy Constructor called\n";
 }	
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
 {
-	std::cout << "Copy Assignment Operator called\n";
+	std::cout << "Bureaucrat Copy Assignment Operator called\n";
 	if (this != &other)
 	{
 		this->grade_ = other.grade_;
@@ -33,17 +33,17 @@ Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
 
 Bureaucrat::~Bureaucrat()
 {
-	std::cout << "Destructor called\n";
+	std::cout << "Bureaucrat Destructor called\n";
 }
 
 Bureaucrat::Bureaucrat(const std::string& name) : name_(name), grade_(_worstGrade)
 {
-	std::cout << "Name Constructor called\n";
+	std::cout << "Bureaucrat Name Constructor called\n";
 }
 
 Bureaucrat::Bureaucrat(const std::string& name, int grade) : name_(name), grade_(isValidGrade_(grade))
 {
-	std::cout << "Name and Grade Constructor called\n";
+	std::cout << "Bureaucrat Name and Grade Constructor called\n";
 }
 
 const std::string& Bureaucrat::getName() const
--- 05md/ex02/main.cpp
+++ 05md/ex02/main.cpp
@@ -4,6 +4,48 @@
 
 int	main(void)
 {
-	ShrubberyCreationForm	a;
-	std::cout << a << std::endl;
+	ShrubberyCreationForm	scf;
+	std::cout << "\n\n\n\n";
+	std::cout << scf << std::endl;
+	std::cout << "\n\n\n\n";
+	Bureaucrat b1;
+	std::cout << "\n\n\n\n";
+	std::cout << b1 << std::endl;
+	std::cout << "\n\n\n\n";
+	try
+	{
+		b1.signForm(scf);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "\n\n\n\n";
+	ShrubberyCreationForm scf2(scf);
+	std::cout << "\n\n\n\n";
+	std::cout << scf2 << std::endl;
+	std::cout << "\n\n\n\n";
+
+	std::cout << scf2.getTarget() << "-TARGET\n\n\n";
+
+	ShrubberyCreationForm scf3("home");
+	std::cout << "\n\n\n\n";
+	std::cout << scf3 << std::endl;
+	std::cout << "\n\n\n\n";
+
+	std::cout << scf3.getTarget() << "-TARGET\n\n\n";
+
+	Bureaucrat b2("El Repsidente", 1);
+	std::cout << "\n\n\n\n";
+	std::cout << b2 << std::endl;
+	std::cout << "\n\n\n\n";
+	try
+	{
+		b2.signForm(scf);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
 }
